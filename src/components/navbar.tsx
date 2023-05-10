8cd16a8002ac070185572e4bdda84b40eb88adb1
Mohit Ramani <ramanimohit01@gmail.com>
2023-05-10 19:40:46 +0530

    chore: add session provider

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,28 +8,6 @@ import { cn } from "@/lib/utils"
 import { ModeToggle } from "./mode-toggle"
 import { buttonVariants } from "./ui/button"
 
-{
-  /* <div className="flex flex-col min-h-screen">
-      <header className="container z-40 bg-background">
-        <div className="flex items-center justify-between h-20 py-6">
-          <nav>
-            <ModeToggle />
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-            >
-              Login
-            </Link>
-          </nav>
-        </div>
-      </header>
-      <main className="flex-1"></main>
-    </div> */
-}
-
 export function Navbar() {
   return (
     <header className="container z-40 bg-background">
